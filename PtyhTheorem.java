04cb309684e3627a5df4dc5152a367188801f0e7
Robert Santos <rasbms81608@gmail.com>
2020-09-27 15:32:47 -0500

    inital commit

---
+++ PtyhTheorem.java
@@ -0,0 +1,9 @@
+public class PtyhTheorem {
+    public double calculateHypotenuse(int legA, int legB) {
+        double a = legA * legA;
+        double b = legB * legB;
+        double c = a + b;
+        double squareRoot = Math.sqrt(c);
+        return squareRoot;
+    }
+}
---
+++ PtyhTheoremTest.java
@@ -0,0 +1,7 @@
+public class PtyhTheoremTest {
+    public static void main(String[] args){
+        PtyhTheorem iD = new PtyhTheorem();
+        double currentSum = iD.calculateHypotenuse(4,5);
+        System.out.println(currentSum);
+    }
+}
